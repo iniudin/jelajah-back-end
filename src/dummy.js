583187be745a820a3984c41f9193df9d4378487e
Ahmad Syaifudin <sysfdn@gmail.com>
2022-07-01 15:42:25 +0700

    update data

--- src/dummy.js
+++ src/dummy.js
@@ -1,46 +1,50 @@
+const local_missions = [
+  {
+    "id": 1,
+    "planId": 1,
+    "name": "Misi 1",
+    "lat": -7.816072,
+    "long": 110.368462,
+    "status": false
+  },
+    {
+    "id": 2,
+    "planId": 1,
+    "name": "Misi 2",
+    "lat": -7.816072,
+    "long": 110.368462,
+    "status": false
+  }
+],
+
+const local_places = [
+  {
+    "id": 1,
+    "name": "Pantai Parangtritis",
+    "address": "Yogyakarta",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros justo, egestas eu eros quis, dapibus viverra tellus. Fusce id dapibus nisl. Etiam libero erat, sagittis ac lectus ac, elementum varius sem. Vestibulum suscipit eros tellus, ut dignissim leo lacinia rutrum. Morbi sollicitudin eget ex blandit tempus.",
+    "rating": 4.6,
+    "plans": [
+      { "id": 1, "name": "Yogyakarta basic plan" },
+      { "id": 1, "name": "Yogyakarta culinary plan" },
+    ],
+    "image": [
+      "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    ],
+    "coverImage": "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  }
+],
+
 const local_plans =  [
   {
   "id": 1,
   "name": "Yogyakarta basic plan",
   "category": 1,
   "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros justo, egestas eu eros quis, dapibus viverra tellus.",
-  "missions": [
-    {
-      "id": 1,
-      "planId": 1,
-      "name": "Misi 1",
-      "lat": -7.816072,
-      "long": 110.368462,
-      "status": false
-    },
-      {
-      "id": 2,
-      "planId": 1,
-      "name": "Misi 2",
-      "lat": -7.816072,
-      "long": 110.368462,
-      "status": false
-    }
-  ],
-  "places": [
-    {
-      "id": 1,
-      "name": "Pantai Parangtritis",
-      "address": "Yogyakarta",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros justo, egestas eu eros quis, dapibus viverra tellus. Fusce id dapibus nisl. Etiam libero erat, sagittis ac lectus ac, elementum varius sem. Vestibulum suscipit eros tellus, ut dignissim leo lacinia rutrum. Morbi sollicitudin eget ex blandit tempus.",
-      "rating": 4.6,
-      "plans": [
-        { "id": 1, "name": "Yogyakarta basic plan" },
-        { "id": 1, "name": "Yogyakarta culinary plan" },
-      ],
-      "image": [
-        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-      ],
-      "coverImage": "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    }
-  ],
+  "missions": missions, 
+  "places": [],
   "status": false,
 },
 {
@@ -66,25 +70,7 @@ const local_plans =  [
       "status": false
     }
   ],
-  "places": [
-    {
-      "id": 1,
-      "name": "Pantai Parangtritis",
-      "address": "Yogyakarta",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros justo, egestas eu eros quis, dapibus viverra tellus. Fusce id dapibus nisl. Etiam libero erat, sagittis ac lectus ac, elementum varius sem. Vestibulum suscipit eros tellus, ut dignissim leo lacinia rutrum. Morbi sollicitudin eget ex blandit tempus.",
-      "rating": 4.6,
-      "plans": [
-        { "id": 1, "name": "Yogyakarta basic plan" },
-        { "id": 1, "name": "Yogyakarta culinary plan" },
-      ],
-      "image": [
-        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-      ],
-      "coverImage": "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    },
-  ],
+  "places": places,
   "status": false,
 }
 ]
