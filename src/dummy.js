48cc532fd523842b4ea9ce8973b9fe67b41bb927
Ahmad Syaifudin <sysfdn@gmail.com>
2022-07-01 15:34:47 +0700

    update data

--- src/dummy.js
+++ src/dummy.js
@@ -1,22 +1,3 @@
-    
-const local_places = {
-  "id": 1,
-  "name": "Pantai Parangtritis",
-  "address": "Yogyakarta",
-  "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros justo, egestas eu eros quis, dapibus viverra tellus. Fusce id dapibus nisl. Etiam libero erat, sagittis ac lectus ac, elementum varius sem. Vestibulum suscipit eros tellus, ut dignissim leo lacinia rutrum. Morbi sollicitudin eget ex blandit tempus.",
-  "rating": 4.6,
-  "plans": [
-    { "id": 1, "name": "Yogyakarta basic plan" },
-    { "id": 1, "name": "Yogyakarta culinary plan" },
-  ],
-  "image": [
-    "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-  ],
-  "coverImage": "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-}
-
 const local_plans =  [
   {
   "id": 1,
@@ -42,7 +23,23 @@ const local_plans =  [
     }
   ],
   "places": [
-   local_places
+    {
+      "id": 1,
+      "name": "Pantai Parangtritis",
+      "address": "Yogyakarta",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros justo, egestas eu eros quis, dapibus viverra tellus. Fusce id dapibus nisl. Etiam libero erat, sagittis ac lectus ac, elementum varius sem. Vestibulum suscipit eros tellus, ut dignissim leo lacinia rutrum. Morbi sollicitudin eget ex blandit tempus.",
+      "rating": 4.6,
+      "plans": [
+        { "id": 1, "name": "Yogyakarta basic plan" },
+        { "id": 1, "name": "Yogyakarta culinary plan" },
+      ],
+      "image": [
+        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      ],
+      "coverImage": "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    }
   ],
   "status": false,
 },
@@ -70,7 +67,23 @@ const local_plans =  [
     }
   ],
   "places": [
-      local_places,
+    {
+      "id": 1,
+      "name": "Pantai Parangtritis",
+      "address": "Yogyakarta",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eros justo, egestas eu eros quis, dapibus viverra tellus. Fusce id dapibus nisl. Etiam libero erat, sagittis ac lectus ac, elementum varius sem. Vestibulum suscipit eros tellus, ut dignissim leo lacinia rutrum. Morbi sollicitudin eget ex blandit tempus.",
+      "rating": 4.6,
+      "plans": [
+        { "id": 1, "name": "Yogyakarta basic plan" },
+        { "id": 1, "name": "Yogyakarta culinary plan" },
+      ],
+      "image": [
+        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      ],
+      "coverImage": "https://images.unsplash.com/photo-1606820031302-d26dcd6b8990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
   ],
   "status": false,
 }
@@ -94,6 +107,7 @@ const local_bandung_city =
     }
   ]
 };
+
 const local_malang_city = {
   "id": 2,
   "name": "Malang",
@@ -110,6 +124,7 @@ const local_malang_city = {
     }
   ]
 }
+
 const local_yogyajakrta_city = {
   "id": 1,
   "name": "Yogyakarta",
